85331e5e789d1aa830ee5ab4c6994248df9e004e
ArtemChubatyi <stgmdota@gmail.com>
2018-11-20 17:12:33 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var syntax        = 'scss'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync');
+		pug 		  = require('gulp-pug');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+gulp.task('pug', function buildHTML() {
+	return gulp.src('src/*.pug')
+	.pipe(pug())
+	.pipe(gulp.dest('src/'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('styles', function() {
+	return gulp.src('src/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('src/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'src/libs/jquery/dist/jquery.min.js',
+		'src/libs/microlight/microlight.js',
+		'src/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('src/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('src/**')
+	.pipe(rsync({
+		root: 'src/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
+	gulp.watch('src/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'src/js/common.js'], ['js']);
+	gulp.watch('src/**/*.pug', ['pug'], browserSync.reload)
+});
+
+gulp.task('default', ['watch']);
---
+++ src/js/common.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	// ------Sidebar-------
+	$(".sidebar-menu > li.have-children a").on("click", function(){
+    if( ! $(this).parent().hasClass("active") ){
+      $(".sidebar-menu li ul").slideUp();
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+      $(this).parent().addClass("active");
+    }
+    else{
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+        }
+	});
+	
+	// --------Menu------
+
+	var prevScrollPos = window.pageYOffset;
+	window.onscroll = function() {
+		var curentScrollPos = window.pageYOffset;
+		if(prevScrollPos > curentScrollPos) {
+			document.getElementById('header').style.top = "0";
+		} else {
+			document.getElementById('header').style.top = "-80px";
+		}
+		prevScrollPos = curentScrollPos;
+	}
+
+});
