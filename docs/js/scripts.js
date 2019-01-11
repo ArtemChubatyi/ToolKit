5b87e1341dcfe8c2a036781822c9aaafe9214be7
IgnatSemchuk <semchukignat@gmail.com>
2019-01-11 12:38:35 +0200

    worked with lints

---
+++ docs/js/scripts.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+	// ------Sidebar-------
+	$(".sidebar-menu > li.have-children a").on("click", function () {
+		if (!$(this).parent().hasClass("active")) {
+			$(".sidebar-menu li ul").slideUp();
+			$(this).next().slideToggle();
+			$(".sidebar-menu li").removeClass("active");
+			$(this).parent().addClass("active");
+		}
+		else {
+			$(this).next().slideToggle();
+			$(".sidebar-menu li").removeClass("active");
+		}
+	});
+
+	// --------Speed dial------
+	$(".tk-speed-dial__button").on("click", function () {
+		$(".tk-speed-dial ul").slideToggle();
+		$(".tk-speed-dial__item").on("click", function () {
+			$(".tk-speed-dial ul").slideUp();
+		});
+	});
+
+	// --------Menu whith dropdown------
+	$(".tk-menu__item.have-children > .tk-item__content").on("click", function () {
+		if (!$(this).parent().hasClass("active")) {
+			$(this).parent().addClass("active");
+		} else {
+			$(this).parent().removeClass("active");
+		}
+		$(this).next().slideToggle();
+		$(".tk-menu__inner-item").on("click", function () {
+			$(".tk-menu__item ul").slideUp();
+			$(".tk-menu__item").removeClass("active");
+		});
+	});
+
+	// --------modal toast------
+	document.querySelector('.toast-examples > button').onclick = function () {
+		let toast = document.querySelector('.toast-examples > .tk-toast');
+		toast.classList.add('tk-toast_show');
+		setTimeout(function () {
+			toast.classList.remove('tk-toast_show');
+		}, 4000);
+	}
+
+	// --------modal dialog------
+	let modal = document.querySelector('[data-modal="dialog"]');
+	let btn = document.querySelector('[data-onclick="dialog"]');
+	let overflow = document.createElement('div');
+	overflow.className = "overflow";
+
+	btn.onclick = function () {
+		document.body.appendChild(overflow);
+		modal.style.display = "flex";
+	}
+
+	overflow.onclick = function () {
+		overflow.remove();
+		modal.style.display = "none";
+	}
+
+	// --------Top bar------
+	var prevScrollPos = window.pageYOffset;
+	window.onscroll = function () {
+		var curentScrollPos = window.pageYOffset;
+		if (prevScrollPos > curentScrollPos) {
+			document.getElementById('header').style.top = "0";
+		} else {
+			document.getElementById('header').style.top = "-80px";
+		}
+		prevScrollPos = curentScrollPos;
+	}
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,85 @@
-var syntax        = 'scss'; // Syntax: sass or scss;
+const syntax = "scss"; // Syntax: sass or scss;
 
-var gulp          = require('gulp'),
-        gutil         = require('gulp-util' ),
-        sass          = require('gulp-sass'),
-        browserSync   = require('browser-sync'),
-        concat        = require('gulp-concat'),
-        uglify        = require('gulp-uglify'),
-        cleancss      = require('gulp-clean-css'),
-        rename        = require('gulp-rename'),
-        autoprefixer  = require('gulp-autoprefixer'),
-        notify        = require("gulp-notify"),
-        rsync         = require('gulp-rsync');
-        pug           = require('gulp-pug');
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const clean = require("gulp-clean");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const cleancss = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
+const notify = require("gulp-notify");
+const rsync = require("gulp-rsync");
+const pug = require("gulp-pug");
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'src'
-        },
-        notify: false,
-        // open: false,
-        // online: false, // Work Offline Without Internet Connection
-        // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
-    });
+gulp.task("browser-sync", () => {
+  browserSync.init({
+    server: {
+      baseDir: "./docs"
+    },
+    notify: false
+    // open: false,
+    // online: false, // Work Offline Without Internet Connection
+    // tunnel: true,
+    // tunnel: "projectname",
+    // Demonstration page: http://projectname.localtunnel.me
+  });
 });
 
-gulp.task('pug', function () {
-    return gulp.src('src/*.pug')
+gulp.task("pug", () =>
+  gulp
+    .src("src/*.pug")
     .pipe(pug())
-    .pipe(gulp.dest('src/'))
-    .pipe(browserSync.stream())
-});
-
-gulp.task('styles', function() {
-    return gulp.src('src/'+syntax+'/**/*.'+syntax+'')
-    .pipe(sass({ outputStyle: 'nested' }).on("error", notify.onError()))
-    .pipe(rename({ suffix: '.min', prefix : '' }))
-    .pipe(autoprefixer(['last 15 versions']))
-    .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream())
-});
-
-gulp.task('js', function() {
-    return gulp.src([
-        'src/libs/jquery/dist/jquery.min.js',
-        'src/libs/microlight/microlight.js',
-        'src/libs/ripple/ripple.min.js',
-        'src/js/common.js', // Always at the end
-        ])
-    .pipe(concat('scripts.min.js'))
-    // .pipe(uglify()) // Mifify js (opt.)
-    .pipe(gulp.dest('src/js'))
+    .pipe(gulp.dest("docs/"))
     .pipe(browserSync.reload({ stream: true }))
+);
+
+gulp.task("styles", () =>
+  gulp
+    .src(`src/${syntax}/*.${syntax}`)
+    .pipe(sass({ outputStyle: "expanded" }).on("error", notify.onError()))
+    .pipe(rename({ suffix: ".min", prefix: "" }))
+    .pipe(autoprefixer(["last 15 versions"]))
+    .pipe(cleancss({ level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+    .pipe(gulp.dest("docs/css/"))
+    .pipe(browserSync.stream())
+);
+
+gulp.task("js", () =>
+  gulp
+    .src("src/js/**/*.js") // Always at the end
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("docs/js/"))
+    .pipe(browserSync.reload({ stream: true }))
+);
+
+gulp.task("images", () =>
+  gulp
+    .src("src/img/**")
+    .pipe(gulp.dest("docs/img"))
+    .pipe(browserSync.reload({ stream: true }))
+);
+
+gulp.task("fonts", () =>
+  gulp
+    .src("src/fonts/**")
+    .pipe(gulp.dest("docs/fonts/"))
+    .pipe(browserSync.reload({ stream: true }))
+);
+
+gulp.task("libs", () =>
+  gulp
+    .src(["src/libs/**"])
+    .pipe(gulp.dest("docs/libs/"))
+    .pipe(browserSync.reload({ stream: true }))
+);
+
+gulp.task("watch", ["pug", "styles", "js", "fonts", "images", "libs"], () => {
+  gulp.watch("src/index.pug", ["pug"]);
+  gulp.watch(`src/**/*.${syntax}`, ["styles"]);
+  gulp.watch(["src/js/**/*.js"], ["js"]);
 });
 
-gulp.task('rsync', function() {
-    return gulp.src('src/**')
-    .pipe(rsync({
-        root: 'src/',
-        hostname: 'username@yousite.com',
-        destination: 'yousite/public_html/',
-        // include: ['*.htaccess'], // Includes files to deploy
-        exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-        recursive: true,
-        archive: true,
-        silent: false,
-        compress: true
-    }))
-});
-
-gulp.task('watch', ['pug', 'styles', 'js', 'browser-sync'], function() {
-    gulp.watch('src/**/*.pug', ['pug'], browserSync.reload({ stream: true }));
-    gulp.watch('src/**/*.'+syntax+'', ['styles']);
-    gulp.watch(['libs/**/*.js', 'src/js/common.js'], ['js']);
-});
-
-gulp.task('default', ['watch']);
+gulp.task("default", ["browser-sync", "watch"]);
