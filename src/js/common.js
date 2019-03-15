22887c3ee84b1b7d834d2bad94b9afe61062c1a4
Ignat Semchuk <semchukignat@gmail.com>
2019-03-15 11:09:21 +0200

    dialog processworked on the dialog component

--- src/js/common.js
+++ src/js/common.js
@@ -35,29 +35,39 @@
 	});
 
 	// --------modal toast------
+
 	document.getElementById('demo-toast').onclick = function () {
 		const toast = document.getElementById('demo-toast').nextElementSibling;
+
 		toast.classList.add('tk-toast_show');
+
 		setTimeout(function () {
 			toast.classList.remove('tk-toast_show');
 		}, 4000);
 	}
 
 	// --------modal dialog------
-	let modal = document.querySelector('[data-modal="dialog"]');
-	let btn = document.querySelector('[data-onclick="dialog"]');
-	let overflow = document.createElement('div');
-	overflow.className = "overflow";
+
+	const modal = document.querySelector('.dialog-demo').lastElementChild;
+	const btn = document.querySelector('.dialog-demo').firstElementChild;
+	const overflow = document.createElement('div');
+
+	overflow.classList.add('overflow');
 
 	btn.onclick = function () {
-		document.body.appendChild(overflow);
-		modal.style.display = "flex";
+		modal.parentElement.appendChild(overflow);
+		modal.classList.add('tk-dialog_show');
+		window.onscroll = function (e) {
+			e.preventDefault();
+			//https://stackoverflow.com/questions/25651448/how-to-preserve-a-spacing-template-on-multiple-lines-using-a-flex-box-container?rq=1
+		}
+	}
+	overflow.onclick = function () {
+		modal.classList.remove('tk-dialog_show');
+		overflow.remove();
 	}
 
-	overflow.onclick = function () {
-		overflow.remove();
-		modal.style.display = "none";
-	}
+
 
 	// --------Top bar------
 	// var prevScrollPos = window.pageYOffset;
